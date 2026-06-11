2fea03feb7c74c89235c0cafcd3550ddbbeaaeaa
SIVA RAMA KISHORE PAKINA <ramasiva4444@gmail.com>
2026-06-11 22:39:42 +0530

    Update mux_2to1.v

--- Verilog-RTL-Designs/2x1 MUX/mux_2to1.v
+++ Verilog-RTL-Designs/2x1 MUX/mux_2to1.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 31.12.2025 12:31:24
-// Design Name: 
-// Module Name: mux_2to1
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module mux_2to1(
 input i0,i1,sel,
